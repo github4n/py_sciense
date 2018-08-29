567743c44eaa6ccf363bb2eee2ffc10c375be41e
wpzero <524162910@qq.com>
2018-08-29 19:02:54 +0800

    添加在ipython中跑的task示例

---
+++ task.py
@@ -0,0 +1,13 @@
+import stock_pool as sp
+import pandas as pd
+import datetime
+import talib as ta
+import numpy as np
+
+sp.produce_sepa_codes(3)
+
+sp.basic_info_step1_for_sepa('2018-08-27_60_sepa_step1_codes', 2018, 2)
+
+sp.basic_info_step2_for_sepa('2018-08-27_60_sepa_step1_codes', 2018, 2)
+
+sp.basic_info_step_for_sepa('2018-08-28_80_sepa_step1_codes', 2018, 2)
