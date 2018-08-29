b62c1b42a97388e67a91dd23ef860abf7c2ae635
wpzero <524162910@qq.com>
2018-08-29 21:22:16 +0800

    添加一些查看大单的功能

--- create_records.sql
+++ create_records.sql
@@ -9,3 +9,14 @@ CREATE TABLE `m_records` (
   KEY `index_m_records_on_code` (`code`),
   KEY `index_m_records_on_status` (`status`)
 ) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `m_stocks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) NOT NULL,
+  `price` float NOT NULL,
+  `count` int(11) DEFAULT '0',
+  `status` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `index_m_stocks_on_code` (`code`),
+  KEY `index_m_stocks_on_status` (`status`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
