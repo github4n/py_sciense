989e3505bcdda6cce473bd91a0f27821396cfd84
wpzero <524162910@qq.com>
2018-09-09 22:07:38 +0800

    完成基本的回测结构代码

--- backtest.py
+++ backtest.py
@@ -62,20 +62,77 @@ class DayBacktest:
         '''
         return data_pool.get_profit_data_df(code, date)
 
+    def get_growth_data_df(self, code, date):
+        '''
+        获得股票的增长信息的dataframe
+        '''
+        return data_pool.get_growth_data_df(code, date)
+
 
 class SepaDayBacktest(DayBacktest):
     def handle_bar(self, date):
         '''
         每个时间窗口的处理函数
         '''
-    def sepa_step_check(self, code, date, days):
+        # 卖昨天指定需要卖的票
+        self.sell_stocks(date)
+        # 买昨天指定需要买的票
+        self.buy_stocks(date)
+
+        self.stocks_pool = self.get_stocks_pool_thru_trend(date)
+
+        # 第二天需要卖掉的股票
+        self.sell_stocks_pool = self.get_sell_stocks_pool_thru_short_trend(date)
+        # 第二天需要买的股票
+        self.buy_stocks_pool = self.get_buy_stocks_pool_thru_short_trend(date)
+
+    def sell_stocks(date):
         '''
-        1. 200天均线是递增的，并且坚持了days天数据
-        2. 当前收盘价大于五十天局限的值
-        3. 五十天天均线值大于一百五十天的均线值
-        4. 一百五十天的均线值大于两百天的均线值
-        <5> 当前股价至少出于一年内最高股价的25%以内
-        <6> 当前股价至少比最近一年最低股价至少高30%
+        卖掉指定股票
+
         '''
-        df = self.get_hist_data(code, date)
-        return basic_method.sepa_step_check(df, days)
+
+    def buy_stocks(date):
+        '''
+        买股票
+        '''
+
+    def get_stocks_pool_thru_trend(self, date):
+        '''
+        根据长期sepa和基本面趋势模型
+        获得股票池
+        '''
+        all_codes_df = self.get_all_codes_df()
+        data = []
+        labels = ['code', 'name', 'cname']
+        indexes = []
+        for index, row in all_codes_df.iterrows():
+            hist_df = self.get_hist_data_df(row['code'], date)
+            growth_df = self.get_growth_data_df(row['code'], date)
+            if basic_method.sepa_step_check(hist_df, 3 * 20) and basic_method.growth_check(growth_df):
+                data.append((row['code'], row['name']), row['cname'])
+                indexes.append(row['code'])
+        df = pd.DataFrame.from_records(data, columns=labels, index=indexes)
+        return df
+
+    def score_stocks_thru_short_trend(self, date):
+        '''
+        根据短期的趋势来给股票打分
+        '''
+        df = self.stocks_pool.copy()
+        # 打分的操作
+        return df
+
+    def get_sell_stocks_pool_thru_short_trend(self, date):
+        '''
+        获得明天需要卖掉的股票
+        '''
+
+    def get_buy_stocks_pool_thru_short_trend(self, date):
+        '''
+        获得明天需要买的股票
+        '''
+        df = self.score_stocks_thru_short_trend(date)
+        df = df.sort_values(by=['score'], ascending=False)
+        df = df.loc[df['code'] >= 8]
+        return df
